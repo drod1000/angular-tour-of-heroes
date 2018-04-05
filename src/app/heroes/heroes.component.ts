9292ea202ed4fd9035ca844bfbeffafc30d6e75a
Daniel Rodriguez <drod2793@gmail.com>
2018-04-04 19:33:05 -0600

    8: Makes spacing consistent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,6 +8,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
   heroes : Hero[];
 
   constructor(private heroService: HeroService) { }
